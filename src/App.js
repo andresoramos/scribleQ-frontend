fcdb687c78401ac6a7e391199898fde65025a61f
Andres Ramos <andresoramos@gmail.com>
2020-05-29 18:48:28 +0100

    Working on NavBar

--- src/App.js
+++ src/App.js
@@ -2,19 +2,23 @@ import React, { useState } from "react";
 import "./App.css";
 import { Route, Switch, Redirect } from "react-router-dom";
 import Register from "./Components/Register";
+import NavBar from "./Components/NavBar";
 
 function App() {
   const [test, setTest] = useState("butt bule");
 
   return (
-    <Switch>
-      <Route
-        path="/"
-        render={(props) => {
-          return <Register test={"This test worked"} />;
-        }}
-      />
-    </Switch>
+    <React.Fragment>
+      <NavBar />
+      <Switch>
+        <Route
+          path="/"
+          render={(props) => {
+            return <Register test={"This test worked"} />;
+          }}
+        />
+      </Switch>
+    </React.Fragment>
   );
 }
 
--- src/Tests/Register.test.js
+++ src/Tests/Register.test.js
@@ -1,22 +1,31 @@
+import React from "react";
+import { render, fireEvent, waitFor, screen } from "@testing-library/react";
+import "@testing-library/jest-dom/extend-expect";
+import axiosMock from "axios";
+import Register from "../Components/Register";
+
 import { validate } from "../Components/Register";
 import Joi, { describe } from "joi";
 
-// describe("Validation on submit", () => {
-it("Validate should return null for valid user object", () => {
-  const data = {
-    username: "andresoramos",
-    email: "andresoramos@gmail.com",
-    password: "Anus1987",
-  };
-  const schema = {
-    username: Joi.string().required(),
-    email: Joi.string().email().required(),
+jest.mock("axios");
 
-    password: Joi.string().min(5).required(),
-  };
-  const result = validate(data, schema);
-  console.log(result, "This is your result");
-  expect(result).toBe(null);
+// describe("Validation", () => {
+it("should show an error when email is blank", () => {
+  const { queryByLabelText, getByLabelText } = render(<Register></Register>);
+
+  // fireEvent.click(screen.getByTestId("submit"));
+  fireEvent.change(screen.getByTestId("email"), { target: { value: "a" } });
+  fireEvent.change(screen.getByTestId("email"), { target: { value: "" } });
+
+  // expect(container.querySelector("#email-helper-text")).toContain(
+  //   "not allowed to be empty"
+  // );
+
+  expect(screen.getByTestId("sample").innerHTML).toMatch("Sample");
+  expect(screen.getByTestId("email").innerHTML).toMatch(
+    "not allowed to be empty"
+  );
+  // expect(getByText("not allowed to be empty")).toBeInTheDocument();
 });
 
 it("Validate should return an errors object if user object is invalid", () => {
